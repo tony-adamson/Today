112f4f4b4e79cf74482c4cb07ae3c66fdc60d822
Anton <antony.adamson@gmail.com>
2023-09-09 12:53:37 +0600

    Section 5. Configure the data source

--- Today/ReminderListViewControllerCollectionViewController.swift
+++ Today/ReminderListViewControllerCollectionViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class ReminderListViewControllerCollectionViewController: UICollectionViewController {
 
+    /**
+     Добавим псевдоним типа для разнояемого источника данных.
+     */
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource: DataSource!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
 
+        /**
+         Регистрация ячейки определяет, как настроить содержимое и внешний вид ячейки.
+         */
+        let cellRegistration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
     }
     /**
      функция создает новую переменную конфигурации списка со сгруппированным внешним видом.
