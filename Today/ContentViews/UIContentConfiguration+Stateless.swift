5a815963774062f4f1dfb1cdd49de841d645cc98
Anton <antony.adamson@gmail.com>
2023-09-13 20:40:33 +0600

    Using content views

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Антон Адамсон on 13.09.2023.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
